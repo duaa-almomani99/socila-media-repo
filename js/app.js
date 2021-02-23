c4f9bd8a580673412cb70e9daafe894d4437b2e1
Duaa momani <duaa_almomani99@yahoo.com>
2021-02-23 16:45:09 +0200

    finshed

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var age=17;
+var name="duaa";
+var favcolor="green";
+
+var ageuserinput=prompt("try to guess the age ");
+var nameuserinput=prompt("try to guess the name");
+var favcoloruserinput=prompt("what do you think is my fav color?");
+var x =prompt("what is your name")
+
+
+if (ageuserinput >18){
+    alert("welcome to you " + x);
+}else{
+    alert("hello " + x)
+}
